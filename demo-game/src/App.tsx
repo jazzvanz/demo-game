4d38b282c6997f692c2f460aa000eb1a9f017178
Jazz Vangeel <jazzvangeel@jazzs-mbp.home>
2025-02-02 15:08:59 -0500

    adding minor code reduction and usecallbackk

--- demo-game/src/App.tsx
+++ demo-game/src/App.tsx
@@ -1,15 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { intialPanelValues, winningCombinations, Panel } from './constants/constants'
 import { Board } from './components/index'
-
-// WHEN ITS A DRAW !!! - 
-// Accessibilty use <div aria-live="polite">{getStatusMessage()}</div>
 
 function App() {
   const [panelsValues, setPanelsValues] = useState<Panel[]>(intialPanelValues);
   const [isPlayerX, setIsPlayerX] = useState<boolean>(true);
   const [isDraw, setIsDraw] = useState<boolean>(false);
   const [winner, setWinner] = useState<null | string>(null);
+
+  const checkWinner = useCallback((selectedPanels: Panel[], numberSet: string[], player: string) => { 
+    const selectedByPlayer = selectedPanels.filter(({ number }) => numberSet.includes(number));
+    if (selectedByPlayer.length === 3) setWinner(player);
+    return winner
+
+  },[winner])
 
   useEffect(() => {
     if (!winner && !isDraw) return 
@@ -43,7 +47,7 @@ function App() {
 
     if (selectedByPlayerOne.length < 3 && selectedByPlayerTwo.length < 3) return 
 
-    // // If no nulls are left, its a draw - no need to check 
+    // Checking for Draw
     if (selectedPanels.length === 9) { 
       setIsDraw(true);
     }
@@ -51,13 +55,10 @@ function App() {
     for (let i = 0; i < winningCombinations.length; i++) { 
       const winningSet = winningCombinations[i]
 
-      const playerOne = selectedByPlayerOne.filter(({ number }) => winningSet.includes(number))
-      const playerTwo = selectedByPlayerOne.filter(({ number }) => winningSet.includes(number))
-
-      if (playerOne.length === 3) setWinner('X');
-      if (playerTwo.length === 3) setWinner('Y');
+      checkWinner(selectedByPlayerOne, winningSet, 'X');
+      checkWinner(selectedByPlayerTwo, winningSet, 'Y');
     }
-  }, [panelsValues])
+  }, [checkWinner, panelsValues])
 
   const handleSelectPanel = (panelId: string, playerId: string | null) => { 
     const updatedPanelsDetails = panelsValues.map((panel) => { 
@@ -80,8 +81,7 @@ function App() {
     <div className="bg-[#16404D] w-full min-h-screen p-8">
       <div className="max-w-xl w-full mx-auto bg-[#DDA853] p-4 shadow-lg rounded-md">
 
-        {/* Player Tracker */}
-        <div className="flex justify-center  p-6">
+        <div className="flex justify-center  p-6" aria-live="polite">
           <h2>Player Turn: Player {isPlayerX ? 'X' : 'Y'}</h2>
         </div>
 
@@ -91,10 +91,10 @@ function App() {
               <button
                 key={number}
                 onClick={() => handleSelectPanel(number, isPlayerX ? 'X' : 'Y')}
-                aria-label={`Sqaure Number${Number}: ${selected ? 'selected by player' : 'available to select'}`}
-                disabled={selected !== null }
+                aria-label={`Sqaure Number${Number}: ${selected ? 'selected by player ' + selected : 'available to select'}`}
+                disabled={selected !== null || winner !== null}
                 className="bg-[#FBF5DD] shadow-lg hover:bg-[#FBF5DD]/60 focus:bg-[#FBF5DD]/60 active:bg-[#FBF5DD]/60 disabled:bg-[#FBF5DD]/60 rounded-md aspect-square">
-                {number} - {selected}
+                  <span aria-hidden={true}>{selected}</span>
               </button> 
             )
           })}
